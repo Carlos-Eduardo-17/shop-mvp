4843dde4eb722b18162638a72b1fcaa1a23da657
Carlos <carlos.1710.ml@gmail.com>
2026-07-14 19:50:21 -0500

    Middlewares v1.2
    - Se definió el middleware validator para controlar en primera línea la entrada de datos del cliente.
    - Validaciones que cubre: campos faltantes, longitud de cadena, contenido de caracteres especiales, formato de email, minúsculas y/o mayúsculas existentes.
    - Mutaciones a datos que cubre: conversión automática a minúsculas y/o mayúsculas, eliminación de espacios vacíos al inicio y al final de la cadena.
    - Se usó el paquete express-validator.
    - Necesitará de validadores (validators) para cada entidad que requiera utilizarlo.

---
+++ src/middlewares/validator.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+
+export const validateRequest = (req: Request, res: Response, next: NextFunction): void => {
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) { // Si hay errores de formato
+    res.status(400).json({
+      error: 'VALIDATION_ERROR',
+      messages: errors.array().map(err => err.msg)
+    });
+    return;
+  }
+
+  next(); // Si todo está bien, pasar al Controlador
+};
