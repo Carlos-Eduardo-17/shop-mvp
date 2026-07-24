d90889ccd0c595e80ea93206c5cc4cdd3a0e990b
Carlos <carlos.1710.ml@gmail.com>
2026-07-24 18:35:39 -0500

    Utilidades v.1.4
    Utilidad trabajada: appError
    - Se creó la clase validationError para tipificar mejor errores provenientes de los validadores.

--- src/utils/appError.util.ts
+++ src/utils/appError.util.ts
@@ -10,3 +10,17 @@ export class AppError extends Error {
         Error.captureStackTrace(this, this.constructor);
     }
 }
+
+
+export class ValidationError extends Error {
+    public readonly statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.name = "ValidationError";
+
+        // Mantiene el stack trace limpio
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
