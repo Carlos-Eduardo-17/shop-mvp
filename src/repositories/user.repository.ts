bbe14e97617799e8a75031f145a7f5cb8e16b2f2
Carlos <carlos.1710.ml@gmail.com>
2026-07-29 12:55:26 -0500

    Módulo de usuario v.5.6
    Repositorio trabajado: user.repository.ts
    - Se corrigieron los comentarios de retorno de cada método.
    - Se definió void como retorno para delete.

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,12 +2,23 @@
 Repositorio de User
 - Solo llamadas a Prisma, sin try/catch ni throws.
 - Los errores(Excepciones) suben solos al Controller.
+
 Tipos de return
-- create → ✅ Promise<User> | ❌ Excepción
-- find → ✅ Promise<User> | ❌ Promise<User>
-- findMany → ✅ Promise<User>[] | ❌ Promise<User>[] (vacío)
-- update → ✅ Promise<User> | ❌ Excepción
-- delete → ✅ Promise<User> | ❌ Excepción
+
+- create → Siempre devuelve el registro creado (o lanza excepción si Prisma falla)
+✅ Promise<User> | ❌ Excepción
+
+- find → Puede no existir, por eso el null. lanza excepción si Prisma falla
+✅ Promise<User | null> | ❌ ???
+
+- findMany → Siempre devuelve un array (vacío si no hay resultados, nunca null). lanza excepción si Prisma falla
+✅ Promise<User[]> | ❌ ???
+
+- update → Devuelve el registro actualizado. Si el registro no existe, Prisma lanza una excepción
+✅ Promise<User> | ❌ Excepción
+
+- delete → Casi nunca se requiere ver el objeto eliminado. Si el registro no existe, Prisma lanza excepción (P2025 - Record to delete does not exist). Si el registro está referenciado por FK restrict o sin onDelete: Cascade, Prisma lanza excepción: P2003. Error interno de PRisma lanza excepción: P1001, P1002, etc
+✅ Promise<void> | ❌ Excepción
 */
 
 import { prisma } from '../config/db.js';
@@ -26,8 +37,8 @@ export class UserRepository {
     async update(id: string, data: Prisma.UserUpdateInput): Promise<User> {
         return await prisma.user.update({ where: { id }, data });
     }
-    async delete(id: string): Promise<User> {
-        return await prisma.user.delete({ where: { id } });
+    async delete(id: string): Promise<void> {
+        await prisma.user.delete({ where: { id } });
     }
     async findByEmail(email: string): Promise<User | null> {
         return await prisma.user.findUnique({ where: { email } });
