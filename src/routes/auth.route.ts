a39ff0a87e31307b6dbed9e2941b9575ec626d7b
Carlos <carlos.1710.ml@gmail.com>
2026-08-17 17:33:27 -0500

    Aumentando rate limit para pruebas

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,23 +1,19 @@
-import { Router } from "express";
-
-import { UserController } from '../controllers/user.controller.js'
-import { UserService } from "../services/user.service.js";
-import { UserRepository } from "../repositories/user.repository.js";
-
-import { limitRequests } from "../middlewares/rateLimit.middleware.js";
+import { Router } from 'express';
+import { UserController } from '../controllers/user.controller.js';
+import { limitRequests } from '../middlewares/rateLimit.middleware.js';
+import { requireAuth } from '../middlewares/requireAuth.middleware.js';
 import { validateRequest } from '../middlewares/validator.middleware.js';
-
-import { loginRules, cleaningRules, registerRules } from '../validators/user.validator.js'
-import { requireAuth } from "../middlewares/requireAuth.middleware.js";
-
+import { UserRepository } from '../repositories/user.repository.js';
+import { UserService } from '../services/user.service.js';
+import { cleaningRules, loginRules, registerRules } from '../validators/user.validator.js';
 
 const userController = new UserController(new UserService(new UserRepository()));
 const router = Router();
 
 // 1ro revisará el límite de request, 2do revisará que los campos cumplan con las reglas de validación, 3ro revisará si pasaron correctamente las reglas de validación
-router.post("/register", limitRequests(900, 3), registerRules, validateRequest, userController.register); //300,3
-router.post("/login", limitRequests(900, 5), loginRules, validateRequest, userController.login);
-router.post("/refresh", limitRequests(900, 15), cleaningRules, validateRequest, userController.refresh);
-router.post("/logout", limitRequests(900, 10), cleaningRules, validateRequest, requireAuth, userController.logout);
+router.post('/register', limitRequests(300, 12), registerRules, validateRequest, userController.register); //300,3
+router.post('/login', limitRequests(300, 12), loginRules, validateRequest, userController.login);
+router.post('/refresh', limitRequests(300, 15), cleaningRules, validateRequest, userController.refresh);
+router.post('/logout', limitRequests(300, 12), cleaningRules, validateRequest, requireAuth, userController.logout);
 
 export default router;
